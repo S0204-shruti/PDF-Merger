17abda2ffa711d1bbf3ad7f5c6082faf038dbf79
Shruti Prakash <shrutiprakash0204@gmail.com>
2026-02-08 22:55:55 +0530

    Modify app.run to use dynamic port configuration
    Update main entry point to use environment port.

--- app.py
+++ app.py
@@ -99,6 +99,8 @@ def download_file(filename):
     return send_from_directory(app.config['MERGED_FOLDER'], filename, as_attachment=True)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port)
+
     
